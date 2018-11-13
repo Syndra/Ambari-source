e33f4a50b73d14f9c87ae4a6c42a20c6f8dc7d8c
ObliQ <kdwoo1217@gmail.com>
2018-11-13 18:56:32 +0900

    Test Add JOBS on Test Tab

--- contrib/views/hive20/src/main/resources/ui/app/routes/test.js
+++ contrib/views/hive20/src/main/resources/ui/app/routes/test.js
@@ -18,79 +18,62 @@
 
 import Ember from 'ember';
 
-export default Ember.Route.extend(UILoggerMixin, {
-  model() {
-    return this.store.findAll('setting').then(settings => settings.toArray());
-  },
-  setupController(controller, model) {
-    this._super(...arguments);
-    const appendedHiveParams = this.prepareExhaustiveParameters(hiveParams, model);
-    controller.set('hiveParameters', appendedHiveParams);
-  },
+export default Ember.Route.extend({
+	moment: Ember.inject.service(),
+	query: Ember.inject.service(),
+	timeInitializedTo: null,
+	queryParams: {
+	  startTime: {
+	    refreshModel: true
+	  },
+	  endTime: {
+	    refreshModel: true
+	  }
+	},
 
-  prepareExhaustiveParameters(hiveParams, model) {
-    let newHiveParams = [];
-    newHiveParams.pushObjects(hiveParams);
-    model.forEach(x => {
-      let param = hiveParams.findBy('name', x.get('key'));
 
-      if(Ember.isEmpty(param)) {
-        newHiveParams.pushObject(
-          Ember.Object.create({name: x.get('key'), disabled: true})
-        );
-      } else {
-        param.set('disabled', true);
-      }
-    });
-    return newHiveParams;
-  },
+	model(params) {
+	  let now = this.get('moment').moment();
+	  if(Ember.isEmpty(params.startTime) || Ember.isEmpty(params.endTime)) {
+	    let clone = now.clone();
+	    params.endTime = now.endOf('day').valueOf();
+	    params.startTime = clone.subtract('7', 'days').startOf('day').valueOf();
+	    this.set('startInitTo', params.startTime);
+	    this.set('endInitTo', params.endTime);
+	  }
 
-  actions: {
-    addNewSettings() {
-      let model = this.get('controller.model');
-      model.forEach(x => x.rollbackAttributes());
-      let newItem = this.store.createRecord('setting', {editMode: true});
-      model.pushObject(newItem);
-    },
+	  return this.store.query('job', params);
+	},
 
-    editAction(setting) {
-      setting.set('editMode', true);
-    },
+	setupController(controller) {
+	  if(!(Ember.isEmpty(this.get('startInitTo')) || Ember.isEmpty(this.get('endInitTo')))) {
 
-    deleteAction(setting) {
-      return setting.destroyRecord().then(data => {
-        let model = this.get('controller.model');
-        model.removeObject(data);
-        let hiveParameters = this.controller.get('hiveParameters');
-        let existingHiveParams = hiveParameters.findBy('name', setting.get('key'));
-        if(existingHiveParams) {
-          existingHiveParams.set('disabled', false);
-        }
-      }, err => {
-        this.get('logger').danger(`Failed to delete setting with key: '${setting.get('key')}`, this.extractError(err));
-      });
-    },
+	    controller.set('endTime', this.get('endInitTo'));
+	    controller.set('startTime', this.get('startInitTo'));
+	    //unset timeInitializedTo
+	    this.set('endInitTo');
+	    this.set('startInitTo');
+	  }
 
-    updateAction(newSetting) {
-      newSetting.save().then(data => {
-        data.set('editMode', false);
-      }, error => {
-        this.get('logger').danger(`Failed to update setting with key: '${newSetting.get('key')}`, this.extractError(error));
-      });
-    },
+	  this._super(...arguments);
 
-    cancelAction(newSetting) {
-      if (newSetting.get('isNew')) {
-        let model = this.get('controller.model');
-        model.removeObject(newSetting);
-      } else {
-        newSetting.set('editMode', false);
-      }
-    },
+	},
 
-    willTransition(transition) {
-      let unsavedModels = this.get('controller.model').filterBy('isNew', true);
-      unsavedModels.forEach(x => this.store.unloadRecord(x));
-    }
-  }
+	actions: {
+	  dateFilterChanged(startTime, endTime) {
+	    this.controller.set('startTime', this.get('moment').moment(startTime, 'YYYY-MM-DD').startOf('day').valueOf());
+	    this.controller.set('endTime', this.get('moment').moment(endTime, 'YYYY-MM-DD').endOf('day').valueOf());
+	    this.refresh();
+	  },
+	  openWorksheet(worksheet, isExisitingWorksheet) {
+	    if(isExisitingWorksheet) {
+	     this.transitionTo('queries.query', worksheet.id);
+	     return;
+	    }
+	    this.get("store").createRecord('worksheet', worksheet );
+	    this.controllerFor('queries').set('worksheets', this.store.peekAll('worksheet'));
+	    this.transitionTo('queries.query', worksheet.id);
+	    this.controllerFor("queries.query").set('previewJobData', {id:worksheet.id, title:worksheet.title.toLowerCase()});
+	  }
+	}
 });
