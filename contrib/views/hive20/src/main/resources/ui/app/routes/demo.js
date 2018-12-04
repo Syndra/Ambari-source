2b7bbb61a3d1c7bde72dd894181df760dd03f4c2
ObliQ <kdwoo1217@gmail.com>
2018-12-04 22:37:52 +0900

    Prepare Demo 2

--- contrib/views/hive20/src/main/resources/ui/app/routes/demo.js
+++ contrib/views/hive20/src/main/resources/ui/app/routes/demo.js
@@ -19,10 +19,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	actions: {
-		gotoSuperset(){
-     	 	var win = window.open("http://13.124.73.187:9088/superset/sqllab", '_blank');
-      		win.focus();
-    	}
-    }
+	
 });
